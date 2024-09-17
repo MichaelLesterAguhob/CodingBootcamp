24eb45040f835390f3bf62461d3591e651332e13
michaellesteraguhob023 <michaellesteraguhob023@gmail.com>
2024-09-17 16:56:26 +0800

    Added s23 Activity codes

---
+++ backend/s23/discussion/index.js
@@ -0,0 +1,18 @@
+console.log("World Hello");
+
+// Arithmetic Operators + - / *
+let x = 1397;
+let y = 7931;
+let sum = x + y;
+console.log("sum = " + sum);
+
+let diff = y - x;
+console.log("diff = " + diff);
+
+let prod = y * x;
+console.log("prod = " + prod);
+
+let qout = y / x;
+console.log("quot = " + qout);
+
+
